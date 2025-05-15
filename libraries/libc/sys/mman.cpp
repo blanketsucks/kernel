6af7226dadc7af57a3fa6b7df60f18bfac355065
blanketsucks <boyc9327@gmail.com>
2025-05-15 20:43:24 +0100

    Properly handle the return value of sys$mmap from libc

--- libraries/libc/sys/mman.cpp
+++ libraries/libc/sys/mman.cpp
@@ -1,11 +1,19 @@
 #include <sys/mman.h>
 #include <sys/syscall.hpp>
+#include <errno.h>
 
 extern "C" {
 
 void* mmap(void* addr, size_t size, int prot, int flags, int fd, off_t offset) {
     mmap_args args = { addr, size, prot, flags, fd, offset };
-    return reinterpret_cast<void*>(syscall(SYS_MMAP, reinterpret_cast<uintptr_t>(&args)));
+    uintptr_t ret = syscall(SYS_MMAP, reinterpret_cast<uintptr_t>(&args));
+
+    if (ret < 0) {
+        errno = -ret;
+        return MAP_FAILED;
+    } else {
+        return reinterpret_cast<void*>(ret);
+    }
 }
 
 int munmap(void*, size_t) {
