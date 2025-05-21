f1c83eafc327f3aff215db2e7ade8fcf32e538ac
blanketsucks <boyc9327@gmail.com>
2025-05-22 00:07:18 +0100

    Implement ramfs::FileSystem::create

--- kernel/fs/ramfs/filesystem.h
+++ kernel/fs/ramfs/filesystem.h
@@ -71,7 +71,9 @@ private:
 
 class FileSystem : public fs::FileSystem {
 public:
-    static FileSystem* create();
+    static FileSystem* create() {
+        return new FileSystem();
+    }
 
     fs::FileSystemID id() const override { return fs::FileSystemID::RamFS; }
     StringView name() const override { return "ramfs"; }
