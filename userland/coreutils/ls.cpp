27ac0f2bdb375a64aeca4949877b4d64eb892a85
blanketsucks <boyc9327@gmail.com>
2025-07-12 21:32:17 +0100

    Make ls a bit nicer

--- userland/coreutils/ls.cpp
+++ userland/coreutils/ls.cpp
@@ -6,6 +6,23 @@
 #include <errno.h>
 #include <string.h>
 
+#include <std/vector.h>
+
+struct DirectoryEntry {
+    String name;
+    u8 type;
+};
+
+static void sort_entries(Vector<DirectoryEntry>& entries) {
+    for (size_t i = 0; i < entries.size(); i++) {
+        for (size_t j = i + 1; j < entries.size(); j++) {
+            if (entries[i].type != DT_DIR && entries[j].type == DT_DIR) {
+                std::swap(entries[j], entries[i]);
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     auto* dir = opendir(argc > 1 ? argv[1] : ".");
     if (!dir) {
@@ -13,19 +30,26 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    Vector<DirectoryEntry> entries;
     for (;;) {
         struct dirent* entry = readdir(dir);
         if (!entry) {
             break;
         }
-        
-        String line;
-        if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-            line.append('/');
+
+        String name(entry->d_name);
+        u8 type = entry->d_type;
+
+        entries.append({ move(name), type });
+    }
+
+    sort_entries(entries);
+    for (auto& entry : entries) {
+        if (entry.type == DT_DIR && entry.name != "." && entry.name != "..") {
+            entry.name.append('/');
         }
 
-        line.append(entry->d_name);
-        dbgln(line);
+        dbgln("{}", entry.name);
     }
 
     closedir(dir);
