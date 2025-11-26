4ecbd8b315e35bcfdda4ade6e6061cfaed9f8d20
blanketsucks <boyc9327@gmail.com>
2025-11-26 23:02:16 +0100

    Implement some required c++ abi functions

--- libraries/libc/cxxabi.cpp
+++ libraries/libc/cxxabi.cpp
@@ -1,9 +1,10 @@
-
 #include <std/types.h>
 
 #define ATEXIT_MAX_FUNCS 1024
 
 extern "C" {
+
+__extension__ typedef int __guard __attribute__((mode(__DI__)));
 
 struct atexit_func_entry_t {
     void (*func)(void*);
@@ -43,5 +44,15 @@ void __cxa_finalize(void* dso_handle) {
         entry.func = nullptr;
     }
 }
+
+int __cxa_guard_acquire(__guard* g) {
+    return !*(char*)(g);
+}
+
+void __cxa_guard_release(__guard* g) {
+    *(char*)g = 1;
+}
+
+void __cxa_guard_abort(__guard*) {}
 
 }
