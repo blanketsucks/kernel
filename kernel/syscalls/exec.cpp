3230fed4b7542e52fb706d72ee2efbb0f16120c1
blanketsucks <boyc9327@gmail.com>
2026-01-09 15:46:03 +0100

    Make sure local variables get cleaned up properly in exec

--- kernel/syscalls/exec.cpp
+++ kernel/syscalls/exec.cpp
@@ -4,25 +4,27 @@
 namespace kernel {
 
 ErrorOr<FlatPtr> Process::exec(StringView path, ProcessArguments arguments) {
-    auto vfs = fs::vfs();
+    {
+        auto vfs = fs::vfs();
     
-    auto file = TRY(vfs->open(path, O_RDONLY, 0, m_cwd));
-    auto elf = ELF(file);
-
-    auto* process = new Process(m_id, path, false, nullptr, nullptr, m_cwd, move(arguments), m_tty);
-    process->create_user_entry(elf);
-
-    process->m_file_descriptors.resize(m_file_descriptors.size());
-    for (size_t i = 0; i < m_file_descriptors.size(); i++) {
-        process->m_file_descriptors[i] = m_file_descriptors[i];
+        auto file = TRY(vfs->open(path, O_RDONLY, 0, m_cwd));
+        auto elf = ELF(file);
+    
+        auto* process = new Process(m_id, path, false, nullptr, nullptr, m_cwd, move(arguments), m_tty);
+        process->create_user_entry(elf);
+    
+        process->m_file_descriptors.resize(m_file_descriptors.size());
+        for (size_t i = 0; i < m_file_descriptors.size(); i++) {
+            process->m_file_descriptors[i] = m_file_descriptors[i];
+        }
+    
+        process->m_parent_id = m_parent_id;
+    
+        m_parent_id = 0;
+        m_id = -1; // FIXME: Actually replace the current process rather than making a new one
+    
+        Scheduler::add_process(process);
     }
-
-    process->m_parent_id = m_parent_id;
-
-    m_parent_id = -1;
-    m_id = -1; // FIXME: Actually replace the current process rather than making a new one
-
-    Scheduler::add_process(process);
 
     this->kill();
     return -1;
